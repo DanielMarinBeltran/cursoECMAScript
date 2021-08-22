8c5c3f0c54f4b59c5c92cb850c04fde7ff13279e
Daniel Marin <danielmarin998@hotmail.com>
2021-08-22 13:28:37 -0500

    es 8

--- src/es8/index.js
+++ src/es8/index.js
@@ -27,3 +27,32 @@ console.log(values.length);
 const string = 'hello';
 console.log(string.padStart(7, 'hi'));
 console.log(string.padEnd(12,'hi'));
+
+
+const helloWorld =() =>{
+    return new Promise((resolve,reject)=>{
+        (true)
+        ? setTimeout(()=>resolve('helloWorld'),3000)
+        : reject(new Error ('Test Error'))
+    })
+}
+
+const helloAsync =async() =>{
+    const hello = await helloWorld();
+    console.log(hello);
+
+}
+helloAsync();
+
+const another =async() =>{
+    try{
+        const hello = await helloWorld();
+        console.log(hello);
+
+    } catch (error){
+        console.log(error);
+        
+    }
+}
+
+another();
